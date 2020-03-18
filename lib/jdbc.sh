c93fe8c065efa3e8f6db9eb599fa0698287a9097
masashou1 <masashou@7andi.mygbiz.com>
2020-03-18 20:22:16 +0900

    CI NG調査のためログ追加

--- lib/jdbc.sh
+++ lib/jdbc.sh
@@ -82,7 +82,8 @@ echo "set_jdbc_url 4"
 }
 echo "set_jdbc_url end"
 echo "jdbc.sh 1"
-echo "$DATABASE_URL"
+echo "${DATABASE_URL}"
+echo "jdbc.sh test"
 if [ -n "${DATABASE_URL:-}" ]; then
   echo "jdbc.sh 1-1"
   set_jdbc_url "$DATABASE_URL"
